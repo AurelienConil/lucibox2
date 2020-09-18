de9519d5d9438c9c284a5013b92121cf839495f5
aurelienconil <conil.a@hotmail.fr>
2020-09-18 23:31:56 +0100

    autostart

--- script/autostart.sh
+++ script/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sleep 1
 #node /home/patch/open-stage-control/app -l /home/patch/lucibox/machines/6/osc.json -s 127.0.0.1:9999 -o 9998&
-python /media/patch/LUCIBOX/script/python_daemon.py
-#pd -nogui -jack /home/patch/lucibox/machines/6/simple_samplerloop.pd
+#python /media/patch/LUCIBOX/script/python_daemon.py
+pd -nogui -jack /home/patch/Documents/Pd/bip.pd
