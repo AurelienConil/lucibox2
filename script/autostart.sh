f2817ab0d598de5e2e9af329bc0a335298d73471
aurelienconil <conil.a@hotmail.fr>
2020-10-01 05:26:21 +0100

    fileplayer autostart

--- script/autostart.sh
+++ script/autostart.sh
@@ -2,4 +2,4 @@
 sleep 1
 #node /home/patch/open-stage-control/app -l /home/patch/lucibox/machines/6/osc.json -s 127.0.0.1:9999 -o 9998&
 #python /media/patch/LUCIBOX/script/python_daemon.py
-pd -nogui -jack /home/patch/Documents/Pd/bip.pd
+pd -nogui -jack /home/patch/lucibox/myapp/mp/nogui.pd
